7f71846963446d56fb6d82e852a5dfd3ec87b07a
gmoraes <112905369+guilhermemoraessiqueira@users.noreply.github.com>
2023-05-24 17:27:31 -0300

    feat: PUT pacientes created

--- src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -2,10 +2,10 @@ package med.voll.api.paciente;
 
 import med.voll.api.medico.Medico;
 
-public record DadosListagemPaciente(String nome, String email, String cpf) {
+public record DadosListagemPaciente(Long id,String nome, String email, String cpf) {
 
     public DadosListagemPaciente(Paciente paciente){
-        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf());
     }
 
 }

e0cdb2fe833f363246e28f6ceaaf4aaf38bf0bdc
gmoraes <112905369+guilhermemoraessiqueira@users.noreply.github.com>
2023-05-25 18:33:29 -0300

    feat: migration 4, ativo medico, created

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -26,7 +26,7 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(Pageable paginacao){
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @PutMapping
@@ -40,9 +40,8 @@ public class MedicoController {
     @DeleteMapping("/{id}")
     @Transactional
     public void deletar(@PathVariable Long id){
-        repository.deleteById(id);
-
-
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
 
     }
 
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -25,7 +25,10 @@ import med.voll.api.endereco.Endereco;
         @Embedded
         private Endereco endereco;
 
+        private Boolean ativo;
+
         public Medico(DadosCadastroMedico dados) {
+            this.ativo = true;
             this.nome = dados.nome();
             this.email = dados.email();
             this.telefone = dados.telefone();
@@ -44,5 +47,9 @@ import med.voll.api.endereco.Endereco;
             if (dados.endereco() != null) {
                 this.endereco.atualizarInformacoes(dados.endereco());
             }
+        }
+
+        public void excluir() {
+            this.ativo = false;
         }
     }

39a7378e28beb41128a14b272e66e61e1c67bdc9
gmoraes <112905369+guilhermemoraessiqueira@users.noreply.github.com>
2023-06-22 11:34:09 -0300

    feat: cancelamento consulta created

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,7 +1,8 @@
 package med.voll.api.domain.consulta;
 
 import med.voll.api.domain.ValidacaoExcepition;
-import med.voll.api.domain.consulta.validacoes.ValidadorAgendamentoDeConsulta;
+import med.voll.api.domain.consulta.validacoes.agendamento.ValidadorAgendamentoDeConsulta;
+import med.voll.api.domain.consulta.validacoes.cancelamento.ValidadorCancelamentoDeConsulta;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
@@ -12,50 +13,43 @@ import java.util.List;
 
 @Service
 public class AgendaDeConsultas {
- 
+
     @Autowired
     private ConsultaRepository consultaRepository;
+
     @Autowired
     private MedicoRepository medicoRepository;
+
     @Autowired
     private PacienteRepository pacienteRepository;
+
     @Autowired
     private List<ValidadorAgendamentoDeConsulta> validadores;
 
+    @Autowired
+    private List<ValidadorCancelamentoDeConsulta> validadoresCancelamento;
 
     public DadosDetalhamentoConsulta agendar(DadosAgendamentoConsulta dados) {
-
-        if (!pacienteRepository.existsById(dados.idPaciente())){
-            throw new ValidacaoExcepition("Id do paciente informado não existe");
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoExcepition("Id do paciente informado não existe!");
         }
 
-        if(dados.idMedico()!= null && !medicoRepository.existsById(dados.idMedico())){
-            throw new ValidacaoExcepition("Id do medico informado não existe");
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoExcepition("Id do médico informado não existe!");
         }
 
         validadores.forEach(v -> v.validar(dados));
 
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
         var medico = escolherMedico(dados);
-        if(medico == null){
-            throw new ValidacaoExcepition("Não existe medico disponível nesta data");
+        if (medico == null) {
+            throw new ValidacaoExcepition("Não existe médico disponível nessa data!");
         }
+
         var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
 
         return new DadosDetalhamentoConsulta(consulta);
-    }
-
-    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
-        if(dados.idMedico() != null){
-            return medicoRepository.getReferenceById(dados.idMedico());
-        }
-
-        if(dados.especialidade() == null){
-            throw new ValidacaoExcepition("Especialidade é obrigatória quando médico não for escolhido");
-        }
-
-        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 
     public void cancelar(DadosCancelamentoConsulta dados) {
@@ -63,7 +57,23 @@ public class AgendaDeConsultas {
             throw new ValidacaoExcepition("Id da consulta informado não existe!");
         }
 
+        validadoresCancelamento.forEach(v -> v.validar(dados));
+
         var consulta = consultaRepository.getReferenceById(dados.idConsulta());
         consulta.cancelar(dados.motivo());
     }
+
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if (dados.idMedico() != null) {
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null) {
+            throw new ValidacaoExcepition("Especialidade é obrigatória quando médico não for escolhido!");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+    }
+
 }
--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -8,32 +8,33 @@ import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDateTime;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
-
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 
+
     @Query("""
-                select m from Medico m
+            select m from Medico m
+            where
+            m.ativo = 1
+            and
+            m.especialidade = :especialidade
+            and
+            m.id not in(
+                select c.medico.id from Consulta c
                 where
-                m.ativo = 1
+                c.data = :data
                 and
-                m.especialidade = :especialidade
-                and
-                m.id not in(
-                    select c.medico.id from Consulta c
-                    where
-                    c.data = :data
-                )
-                order by rand()
-                limit 1
-                """)
+                c.motivoCancelamento is null
+            )
+            order by rand()
+            limit 1
+        """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
     @Query("""
             select m.ativo
             from Medico m
             where
             m.id = :id
             """)
-
-
-    boolean findAtivoById(Long id);
+    Boolean findAtivoById(Long id);
 }
